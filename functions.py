510813a6fc799a1c1f0c8cc7015a64f085d3f474
daviddias99 <up201705373@fe.up.pt>
2021-04-17 17:25:27 +0100

    Refactored

--- functions.py
+++ functions.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 from sklearn.linear_model import RANSACRegressor, LinearRegression
 
 
-def chessboardIntrinsicCalibration(chessboard_dimensions, frame_path):
+def chessboardPointExtraction(chessboard_dimensions, frame_path):
   # termination criteria
   criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
@@ -46,53 +46,16 @@ def chessboardIntrinsicCalibration(chessboard_dimensions, frame_path):
 def calculateReprojectionError(objpoints, imgpoints, rvecs, tvecs, imatrix, distortion):
   mean_error = 0
   for i in range(len(objpoints)):
-      calculted_image_points, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], imatrix, distortion)
+
+      if(len(rvecs) == 3):
+        calculted_image_points, _ = cv2.projectPoints(objpoints[i], rvecs, tvecs, imatrix, distortion)
+      else:
+        calculted_image_points, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], imatrix, distortion)
       error = cv2.norm(imgpoints[i], calculted_image_points, cv2.NORM_L2)/len(calculted_image_points)
       mean_error += error
 
   return mean_error/len(objpoints)
 
-def chessboardExtrinsicCalibration(chessboard_dimensions, frame_path):
-  # termination criteria
-  criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-
-  # Arrays to store object points and image points from all the images.
-  object_points = [] # 3d point in real world space
-  image_points = [] # 2d points in image plane.
-
-  # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-  sample_object_points = np.zeros((chessboard_dimensions[0] * chessboard_dimensions[1],3), np.float32)
-
-  # TODO: check if this is the correct way to adjust the sizes. Used this accordint to: https://stackoverflow.com/questions/37310210/camera-calibration-with-opencv-how-to-adjust-chessboard-square-size
-  sample_object_points[:,:2] = np.mgrid[0:chessboard_dimensions[0],0:chessboard_dimensions[1]].T.reshape(-1,2) * chessboard_dimensions[2]
-
-  # Swap axis so that the z axis is perpendicular to the chessboard
-  sample_object_points[:, [1, 0]] = sample_object_points[:, [0, 1]]
-
-  images = glob.glob(f'{frame_path}/*.png')
-  print(f'Found {len(images)} calibration images')
-
-  for fname in tqdm(images):
-      image = cv2.imread(fname)
-      grayscale_image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-
-      # Find the chess board corners
-      ret, corners = cv2.findChessboardCorners(grayscale_image, (chessboard_dimensions[0], chessboard_dimensions[1]), None)
-
-      # If found, add object points, image points (after refining them)
-      if ret == True:
-          object_points.extend(list(sample_object_points))
-          improved_corners = cv2.cornerSubPix(grayscale_image, corners, (11,11), (-1,-1), criteria)
-          image_points.extend(list(improved_corners))
-      else:
-          print(f'Could not find chessboard corners in {fname}')
-
-  object_points = np.float64(object_points)
-  image_points = np.float64(image_points)
-  print('Object points shape:', object_points.shape)
-  print('Image points shape:', image_points.shape)
-
-  return object_points, image_points
 
 # Constraints is a list where each element is a list [A,B,C,D] representing a constraint of type Ax + By + Cz = D
 def get_xyz_coords(i, j, ppm, constraints = None):
